693efa6e1c3307944ec3805b621c21f4b3ad2c75
Pedro L.C.A <thezakman@icloud.com>
2023-04-21 04:49:24 -0300

    Update routes.js

--- front-end/src/router/routes.js
+++ front-end/src/router/routes.js
@@ -1,37 +1,32 @@
 import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
 
-const XSSPayloadFireReports = () => import("@/pages/XSSPayloadFireReports.vue");
-const XSSPayloads = () => import("@/pages/XSSPayloads.vue");
-const Settings = () => import("@/pages/Settings.vue");
+const XSSPayloadFireReports = () =>
+    import ("@/pages/XSSPayloadFireReports.vue");
+const XSSPayloads = () =>
+    import ("@/pages/XSSPayloads.vue");
+const Settings = () =>
+    import ("@/pages/Settings.vue");
 
-const routes = [
-  {
+const routes = [{
     path: "/",
     component: DashboardLayout,
     redirect: "/",
-    children: [
-      {
-        path: "",
-        name: "Payload Fire Reports",
-        component: XSSPayloadFireReports
-      },
-      {
-        path: "/xsspayloads",
-        name: "Payloads",
-        component: XSSPayloads
-      },
-      {
-        path: "/collectedpages",
-        name: "Collected Pages",
-        component: CollectedPages
-      },
-      {
-        path: "/settings",
-        name: "Settings",
-        component: Settings
-      },
+    children: [{
+            path: "",
+            name: "Payload Fire Reports",
+            component: XSSPayloadFireReports
+        },
+        {
+            path: "/xsspayloads",
+            name: "Payloads",
+            component: XSSPayloads
+        },
+        {
+            path: "/settings",
+            name: "Settings",
+            component: Settings
+        },
     ]
-  }
-];
+}];
 
 export default routes;
